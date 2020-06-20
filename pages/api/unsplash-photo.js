a70f65f1e2d7005fe5180965608791c1e6b78626
Trezy <tre@trezy.com>
2020-06-20 12:39:55 -0500

    feat(api/unsplash-photo): add more photo filters

--- pages/api/unsplash-photo.js
+++ pages/api/unsplash-photo.js
@@ -12,7 +12,7 @@ import httpStatus from 'helpers/httpStatus'
 
 
 export const handler = async (request, response) => {
-  const result = await fetch('https://api.unsplash.com/photos/random?query=birdhouse', {
+  const result = await fetch('https://api.unsplash.com/photos/random?query=birdhouse&orientation=portrait&content_filter=high', {
     headers: {
       'Accept-Version': 'v1',
       Authorization: `Client-ID ${process.env.unsplashAccessKey}`,
