305c7b155d111ffa1acf633f0505b23f2f18fb18
Trezy <tre@trezy.com>
2020-06-30 11:44:46 -0500

    feat(/api/users/check-username): add endpoint for checking username availability

---
+++ src/pages/api/users/check-username.js
@@ -0,0 +1,71 @@
+// Module imports
+import algoliasearch from 'algoliasearch/lite'
+
+
+
+
+
+// Local imports
+import createEndpoint from 'helpers/createEndpoint'
+import httpStatus from 'helpers/httpStatus'
+
+
+
+
+
+// Local constants
+const algoliaClient = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_API_KEY)
+const profileSearchIndex = algoliaClient.initIndex('profiles')
+
+
+
+
+
+export const handler = async (request, response) => {
+  const { username } = request.query
+  const errors = []
+
+  if (!username) {
+    errors.push('username is required.')
+  }
+
+  if (errors.length) {
+    response.status(httpStatus.UNPROCESSABLE_ENTITY)
+    response.end()
+    return
+  }
+
+  await profileSearchIndex.setSettings({
+    searchableAttributes: [
+      'username',
+      'displayName',
+    ]
+  })
+
+  try {
+    const results = await profileSearchIndex.search(username)
+
+    if (results.hits.some(profile => (profile.username === username))) {
+      response.status(httpStatus.CONFLICT)
+      response.end()
+      return
+    }
+  } catch (error) {
+    response.status(httpStatus.INTERNAL_SERVER_ERROR)
+    response.json({ errors: [error.message] })
+    response.end()
+    return
+  }
+
+  response.status(httpStatus.OK)
+  response.end()
+}
+
+
+
+
+
+export default createEndpoint({
+  allowedMethods: ['get'],
+  handler,
+})
